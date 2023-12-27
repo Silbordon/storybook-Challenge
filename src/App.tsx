7359bf9f1404d293c2a9cb9e72ccf2a9ad649e11
silvina@pixeldigita.com <silvina@pixeldigita.com>
2023-12-27 13:35:58 -0300

    feat: added necessary dependencies such as react, style-component, typescript and storybook

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import styled from 'styled-components'
+
+const Apwrapper = styled.div`
+  background-color: aqua;
+`
+function App() {
+  return (
+    <Apwrapper>
+     <p>hola</p>
+    </Apwrapper>
+  );
+}
+
+export default App;
